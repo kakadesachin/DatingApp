61c1c05f4026b46d542d3f59fecfa12cbb01c803
kakadesachin <kakadesachin@outlook.com>
2021-03-19 02:17:55 +0530

    Initiated Token Service Logic

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
+using API.Services;
 
 namespace API
 {
@@ -31,6 +33,19 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
+            This method is used to implement dependency injection and also to specify 
+            the life cycle of the dependency
+            we have 3 methods to add a service
+            1. AddSingleton (once created it will not dispose of that object till the application shuts down)
+            2. AddScoped this life cycle is scoped as long as the http request is processed which is ideally suitable
+            3. AddTransient to be used when services are created and destroyed as soon as the method is finished executing
+            
+            we will be using AddScoped
+            also note will be injecting the interface along with class, as its a better practice and good for testability
+
+            */
+            services.AddScoped<ITokenService,TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
